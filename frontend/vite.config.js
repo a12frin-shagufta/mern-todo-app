912798b235ff50d3754bd7c5387cd16dadb93b6e
shagufta Afrin <your_email@example.com>
2025-03-25 15:06:49 +0530

    todo-frontend-code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Add base for deployment
   plugins: [
     tailwindcss(),
     react()
   ],
-  server:{
+  server: {
     port: 5173, // Fixed port
-    strictPort: true //
+    strictPort: true // Fail if port is already in use
+  },
+  build: {
+    outDir: 'dist', // Output directory for build
+    sourcemap: false, // Disable sourcemaps in production
   }
 })
